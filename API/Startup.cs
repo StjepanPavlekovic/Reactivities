f6dbcf31e1ba5cabb231e39659d5f398e0131356
Stjepan <stjepan.pavlekovic1@gmail.com>
2020-07-15 18:08:12 +0200

    added axios and stuff

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -43,8 +50,14 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
-
             app.UseRouting();
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
 
             app.UseAuthorization();
 
